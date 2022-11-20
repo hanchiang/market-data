e87451ac223637432f0d5eaaf8f5492ccd0c7074
han chiang <yaphc@hotmail.com>
2022-11-20 15:44:54 +0800

    First commit

---
+++ src/main.py
@@ -0,0 +1,26 @@
+from datetime import date
+from fastapi import FastAPI
+from typing import Optional
+
+from barchart_api import BarChartAPI
+
+app = FastAPI()
+
+options_api = BarChartAPI().options
+
+@app.get("/options/{symbol}")
+async def options_for_ticker(
+    symbol: str, order_dir = '', expiration_type = '', expiration_date: Optional[date] = None,
+    group_by: Optional[str] = '', order_by: Optional[str] = ''
+):
+    data = await options_api.get_options_for_ticker(symbol=symbol, expiration_type=expiration_type,
+        expiration_date=expiration_date, group_by=group_by, order_by=order_by, order_dir=order_dir
+    )
+
+    return {'data': data}
+
+@app.get("/options/{symbol}/expirations")
+async def options_expirations_for_ticker(symbol: str):
+    data = await options_api.get_options_expirations_for_ticker(symbol=symbol)
+
+    return {'data': data}
