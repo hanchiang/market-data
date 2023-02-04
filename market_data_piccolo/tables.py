56ad1def22e0b3d7384b6b01f628e8f5e9467b47
han chiang <yaphc@hotmail.com>
2023-02-04 18:56:19 +0800

    Rearrange table.

--- market_data_piccolo/tables.py
+++ market_data_piccolo/tables.py
@@ -37,6 +37,88 @@ class StockTicker(Table, tablename="stock_ticker"):
         secret=False,
     )
 
+class StockTickerPrice(Table, tablename="stock_ticker_price"):
+    id = BigSerial(
+        null=False,
+        primary_key=True,
+        unique=False,
+        index=False,
+        index_method=IndexMethod.btree,
+        db_column_name="id",
+        secret=False,
+    )
+    symbol = ForeignKey(
+        references=StockTicker,
+        on_delete=OnDelete.no_action,
+        on_update=OnUpdate.cascade,
+        target_column=None,
+        null=True,
+        primary_key=False,
+        unique=False,
+        index=False,
+        index_method=IndexMethod.btree,
+        db_column_name=None,
+        secret=False,
+    )
+    date = Date(
+        null=False,
+        primary_key=False,
+        unique=False,
+        index=False,
+        index_method=IndexMethod.btree,
+        db_column_name=None,
+        secret=False,
+    )
+    open_price = Numeric(
+        digits=stock_price_precision,
+        null=False,
+        primary_key=False,
+        unique=False,
+        index=False,
+        index_method=IndexMethod.btree,
+        db_column_name=None,
+        secret=False,
+    )
+    high_price = Numeric(
+        digits=stock_price_precision,
+        null=False,
+        primary_key=False,
+        unique=False,
+        index=False,
+        index_method=IndexMethod.btree,
+        db_column_name=None,
+        secret=False,
+    )
+    low_price = Numeric(
+        digits=stock_price_precision,
+        null=False,
+        primary_key=False,
+        unique=False,
+        index=False,
+        index_method=IndexMethod.btree,
+        db_column_name=None,
+        secret=False,
+    )
+    close_price = Numeric(
+        digits=stock_price_precision,
+        null=False,
+        primary_key=False,
+        unique=False,
+        index=False,
+        index_method=IndexMethod.btree,
+        db_column_name=None,
+        secret=False,
+    )
+    volume = Integer(
+        null=False,
+        primary_key=False,
+        unique=False,
+        index=False,
+        index_method=IndexMethod.btree,
+        db_column_name=None,
+        secret=False,
+    )
+
 
 class OptionPrice(Table, tablename="option_price"):
     id = BigSerial(
@@ -289,84 +371,3 @@ class OptionPrice(Table, tablename="option_price"):
     )
 
 
-class StockTickerPrice(Table, tablename="stock_ticker_price"):
-    id = BigSerial(
-        null=False,
-        primary_key=True,
-        unique=False,
-        index=False,
-        index_method=IndexMethod.btree,
-        db_column_name="id",
-        secret=False,
-    )
-    symbol = ForeignKey(
-        references=StockTicker,
-        on_delete=OnDelete.no_action,
-        on_update=OnUpdate.cascade,
-        target_column=None,
-        null=True,
-        primary_key=False,
-        unique=False,
-        index=False,
-        index_method=IndexMethod.btree,
-        db_column_name=None,
-        secret=False,
-    )
-    date = Date(
-        null=False,
-        primary_key=False,
-        unique=False,
-        index=False,
-        index_method=IndexMethod.btree,
-        db_column_name=None,
-        secret=False,
-    )
-    open_price = Numeric(
-        digits=stock_price_precision,
-        null=False,
-        primary_key=False,
-        unique=False,
-        index=False,
-        index_method=IndexMethod.btree,
-        db_column_name=None,
-        secret=False,
-    )
-    high_price = Numeric(
-        digits=stock_price_precision,
-        null=False,
-        primary_key=False,
-        unique=False,
-        index=False,
-        index_method=IndexMethod.btree,
-        db_column_name=None,
-        secret=False,
-    )
-    low_price = Numeric(
-        digits=stock_price_precision,
-        null=False,
-        primary_key=False,
-        unique=False,
-        index=False,
-        index_method=IndexMethod.btree,
-        db_column_name=None,
-        secret=False,
-    )
-    close_price = Numeric(
-        digits=stock_price_precision,
-        null=False,
-        primary_key=False,
-        unique=False,
-        index=False,
-        index_method=IndexMethod.btree,
-        db_column_name=None,
-        secret=False,
-    )
-    volume = Integer(
-        null=False,
-        primary_key=False,
-        unique=False,
-        index=False,
-        index_method=IndexMethod.btree,
-        db_column_name=None,
-        secret=False,
-    )
