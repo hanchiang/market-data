305e3bd3b0a0c729e984e461bc09bc9e27b67805
Han Chiang <yaphc@hotmail.com>
2026-03-23 16:29:15 +0800

    Refine local library install and compose startup flow

--- src/db/index.py
+++ src/db/index.py
@@ -1,19 +1,6 @@
-import asyncio
 import multiprocessing
-import os
-import subprocess
 
 from piccolo.engine import engine_finder
-
-async def run_migration():
-    print('Running postgres migration scripts. Wait 1 seconds for timescale db to be ready')
-    await asyncio.sleep(1)
-    result = subprocess.run('piccolo migrations forwards market_data_piccolo --trace', cwd=os.getcwd(), timeout=300,
-                            shell=True, capture_output=True)
-    print(result)
-    print(f'stdout: {result.stdout}')
-    if result.stderr:
-        print(f'stderr: {result.stderr}')
 
 async def start_postgres_connection_pool():
     engine = engine_finder()
